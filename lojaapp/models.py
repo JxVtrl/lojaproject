c0343994585f2e6cdfc0df5d3b12afb1b3360a85
Marcelo Bracet <marcelo.bracet1@gmail.com>
2023-01-28 21:39:32 -0300

    showing on admin

--- lojaapp/models.py
+++ lojaapp/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Cliente(models.Model):
-    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     nome_completo = models.CharField(max_length=100)
     endereco = models.CharField(max_length=200, null=True, blank=True)
     data_cadastro = models.DateTimeField(auto_now_add=True)
