2bd5b7121c0977782b32cc1678a8d8d75d8885c7
Marcelo Bracet <marcelo.bracet1@gmail.com>
2023-01-30 19:05:18 -0300

    commit: showing products on home page

--- lojaapp/views.py
+++ lojaapp/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import *
 
 class HomeView(TemplateView):
     template_name = "home.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['produto_list'] = Produto.objects.all()
+        return context
     
 class SobreView(TemplateView):
     template_name = "sobre.html"
