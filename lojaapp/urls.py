161e059d46593e7e736027d1c997b0727bab1496
Marcelo Bracet <marcelo.bracet1@gmail.com>
2023-02-06 21:59:25 -0300

    adding cart

--- lojaapp/urls.py
+++ lojaapp/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("sobre/", SobreView.as_view(), name="sobre"),
     path("contato/", ContatoView.as_view(), name="contato"),
     path("todos-produtos/", TodosProdutosView.as_view(), name="todosprodutos"),
-    # path("produto/<slug: slug>/", ProdutoDetalhesView.as_view(), name="produtodetalhe")
+    path("produto/<slug:slug>/", ProdutoDetalheView.as_view(), name="produtodetalhe"),
+    path("addcarro-<int:pro_id>/", AddCarroView.as_view(), name="addcarro")
 ]
--- lojaapp/views.py
+++ lojaapp/views.py
@@ -16,9 +16,31 @@ class TodosProdutosView(TemplateView):
         context['todascategorias'] = Categoria.objects.all().order_by('-id')
         return context    
 
-# class ProdutoDetalhesView(TemplateView):
-#     template_name = "produtodetalhe.html"
- 
+class ProdutoDetalheView(TemplateView):
+    template_name = "produtodetalhe.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        url_slug = kwargs['slug']
+        produto = Produto.objects.get(slug=url_slug)
+        produto.visualizacao += 1
+        produto.save()
+        context['produto'] = produto
+        return context
+
+class AddCarroView(TemplateView):
+    template_name = "addprocarro.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        produto_id = kwargs['pro_id']
+        produto_obj = Produto.objects.get(id=produto_id)
+        carro_id = self.request.session.get("carro_id", None)
+        if carro_id:
+            carro_obj = Carro.objects.get(id=carro_id)
+        else:
+            carro_obj = Carro.objects.create(total=0)
+            self.request.session['carro_id'] = carro_obj.id
 
 class SobreView(TemplateView):
     template_name = "sobre.html"
